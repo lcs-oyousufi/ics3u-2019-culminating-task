429a9f1da4999a701d9ce53f394f62800b930a66
Osman Yousufi <oyousufi@lcs.on.ca>
2019-05-29 23:41:03 -0400

    had it ready in the file i was working on before but didn't put it in yet because i was having trouble configuring it

--- scrolling-platform-game /truck.java
+++ scrolling-platform-game /truck.java
@@ -1,19 +1,55 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class truck here.
+ * Write a description of class Truck here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class truck extends Enemies
 {
-    /**
-     * Act - do whatever the truck wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int countmove; 
+    private int movecount = 2;
+    private int truckcount = 100; //increase for less trucks decrease for faster truck rate
+    private int counttruck;
+    
+    public truck()
+    {
+        GreenfootImage truck = getImage();
+        truck.scale(truck.getWidth() +50, truck.getHeight() +20 );
+        
+    }
+
     public void act() 
     {
-        // Add your action code here.
-    }    
+        ObjectinWorld = true;
+        counttruck++;
+        edge();
+        countmove++;
+        newtruck();
+        isTouchingfrogger();
+        if(countmove>= movecount)
+        {
+            move(-10);
+            countmove = 0;
+        }
+    }
+    public void newtruck()
+    {
+        
+            if(counttruck>=truckcount){
+                
+                getWorld().addObject(new truck(), 600, getY());
+                counttruck = 0;
+            }
+        
+    }
+
+    public void edge()
+    {
+        if(isAtEdge()){
+            getWorld().removeObject(this);
+            ObjectinWorld = false;
+        }
+    }
 }
